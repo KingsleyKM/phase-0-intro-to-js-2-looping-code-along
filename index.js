1bb5249f095558650117697618fb6e5a7958fe9d
Kingsley Maina <mainakingsley7@gmail.com>
2023-09-25 12:13:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let people = ["Guadalupe", "Ollie", "Aki"];
+function countDown(){
+    for(let i = 10;i >= 0;i--){
+        console.log(i)
+    }
+}
+function writeCards(names) {
+  let thankYouMessages = [];
+  for (let name of names) {
+    thankYouMessages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  return thankYouMessages;
+}let names = ["Guadalupe", "Ollie", "Aki"];
+let  thankYouMessages = writeCards(names);
+
+console.log(thankYouMessages);
